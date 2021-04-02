e720986d6cb0b019426fb9b79418e2cbcbc630e7
xiexiangyu <xiexiangyu@yunzhangfang.com>
2021-04-02 17:16:02 +0800

    针对IE的兼容polyfill有问题，codesplit会有问题 TODO

--- src/component.js
+++ src/component.js
@@ -2,5 +2,11 @@ export default (text = "Hello world") => {
   const element = document.createElement("div");
   element.className = "rounded bg-red-100 border max-w-md m-4 p-4";
   element.innerHTML = text;
+  element.onclick = () =>
+    import("./lazy")
+      .then((lazy) => {
+        element.textContent = lazy.default; // 针对IE的兼容polyfill有问题，codesplit会有问题 TODO
+      })
+      .catch((err) => console.error(err));
   return element;
 };
